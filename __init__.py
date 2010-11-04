19101e029105bb7b2460b87809b4549715685c29
Mark Wiebe <mwwiebe@gmail.com>
2010-11-03 23:03:04 -0700

    make '<f2' and '>f2' work too

--- __init__.py
+++ __init__.py
@@ -6,9 +6,18 @@ from numpy_half import float16
 
 def add_to_typeDict():
     # Add it to the numpy type dictionary
-    from numpy import typeDict
-    typeDict['float16'] = float16
-    typeDict['f2'] = float16
+    import sys, numpy
+    f16 = numpy.dtype(float16)
+    numpy.typeDict['float16'] = f16
+    numpy.typeDict['f2'] = f16
+    numpy.typeDict['=f2'] = f16
+    if sys.byteorder == 'little':
+        numpy.typeDict['<f2'] = f16
+        numpy.typeDict['>f2'] = f16.newbyteorder('>')
+    else:
+        typeDict['>f2'] = f16
+        numpy.typeDict['<f2'] = f16.newbyteorder('<')
+
 
 add_to_typeDict()
 
