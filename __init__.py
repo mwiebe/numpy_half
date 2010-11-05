3287a0b2e255a2c518dc3332570e6f3b69af1ce0
Mark Wiebe <mwwiebe@gmail.com>
2010-11-05 13:43:10 -0700

    add support for numpy.finfo

--- __init__.py
+++ __init__.py
@@ -2,11 +2,15 @@ from info import __doc__
 
 __all__ = ['float16']
 
+import numpy
 from numpy_half import float16
+
+# Add float16 into the numpy module space
+numpy.float16 = float16
 
 def add_to_typeDict():
     # Add it to the numpy type dictionary
-    import sys, numpy
+    import sys
     f16 = numpy.dtype(float16)
     numpy.typeDict['float16'] = f16
     numpy.typeDict['f2'] = f16
@@ -18,8 +22,40 @@ def add_to_typeDict():
         typeDict['>f2'] = f16
         numpy.typeDict['<f2'] = f16.newbyteorder('<')
 
+def add_to_finfo():
+    # Inject float16 into the finfo cache
+    # by hijacking the normal construction method
+    fi = object.__new__(numpy.finfo)
+    fi.dtype = numpy.dtype(float16)
+    fmt = '%6.4e'
+
+    # The MachAr implementation does not work, hardcode everything...
+    fi.machar = None
+
+    fi.precision = 3
+    fi.resolution = float16(10**-fi.precision)
+    fi.tiny = float16(2.0**-14) # Smallest positive normalized number
+    fi.iexp = 5
+    fi.maxexp = 16
+    fi.minexp = -14
+    fi.machep = -10
+    fi.negep = -11
+    fi.max = float16(65504)
+    fi.min = float16(-65504)
+    fi.eps = float16(2.0**fi.machep)
+    fi.epsneg = float16(2.0**fi.negep)
+    fi.nexp = fi.iexp
+    fi.nmant = 10
+    fi._str_tiny = (fmt % fi.tiny).strip()
+    fi._str_max = (fmt % fi.max).strip()
+    fi._str_epsneg = (fmt % fi.epsneg).strip()
+    fi._str_eps = (fmt % fi.eps).strip()
+    fi._str_resolution = (fmt % fi.resolution).strip()
+
+    numpy.finfo._finfo_cache[fi.dtype] = fi
 
 add_to_typeDict()
+add_to_finfo()
 
 from numpy.testing import Tester
 test = Tester().test
